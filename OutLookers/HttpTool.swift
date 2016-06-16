73af751b70da9d84fec5721ce308adbbdf4d372c
youngkook <youngkook1992@163.com>
2016-06-16 17:15:40 +0800

    HttpTool

--- OutLookers/HttpTool.swift
+++ OutLookers/HttpTool.swift
@@ -18,10 +18,11 @@ public class Server {
     
     class func post(url: String, parameters: [String:AnyObject]?, complete:()->Void) -> Void {
         Alamofire.request(.POST, url, parameters: parameters, encoding: .JSON, headers: nil).responseJSON { (response) in
-            if let data = response.result.value {
-                debugPrint("data \(data)")
-            } else {
-                
+            switch response.result {
+            case .Success(let Value):
+                print("\(Value)")
+            case .Failure(let Error):
+                print("\(Error)")
             }
         }
     }
