b8344496d2f625fc840394a1b7f0f775dac38bfd
youngkook <youngkook1992@163.com>
2016-06-30 20:48:04 +0800

    follow

---
+++ OutLookers/FollowPeopleViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FollowPeopleViewController.swift
+//  OutLookers
+//
+//  Created by Youngkook on 16/6/30.
+//  Copyright © 2016年 weiguanonline. All rights reserved.
+//
+
+import UIKit
+
+class FollowPeopleViewController: YGBaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
