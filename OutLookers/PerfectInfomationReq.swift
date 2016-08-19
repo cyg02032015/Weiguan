99107f44f842445381939a8c8060ffd635d3379e
youngKook <youngKook1992@163.com>
2016-08-20 02:10:45 +0800

    注册 cookie headers 修改网络请求

---
+++ OutLookers/PerfectInfomationReq.swift
@@ -0,0 +1,13 @@
+//
+//  PerfectInfomationReq.swift
+//  OutLookers
+//
+//  Created by C on 16/8/19.
+//  Copyright © 2016年 weiguanonline. All rights reserved.
+//
+
+import UIKit
+
+class PerfectInfomationReq: BaseResponse {
+
+}
