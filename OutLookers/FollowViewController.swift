926f2385161680d64338cc9e055fc4cd3908baf2
songbihai <759464290@qq.com>
2016-09-07 12:15:34 +0800

    完成关注列表，分享，评论，点赞

--- OutLookers/FollowViewController.swift
+++ OutLookers/FollowViewController.swift
@@ -18,6 +18,8 @@ class FollowViewController: YGBaseViewController {
     lazy var lists = [DynamicResult]()
     private var timeStr: String!
     
+    private var share: YGShare!
+    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         if UserSingleton.sharedInstance.isLogin() {
@@ -112,15 +114,126 @@ extension FollowViewController {
         let cell = tableView.dequeueReusableCellWithIdentifier(dynamicCellIdentifier, forIndexPath: indexPath) as! DynamicCell
         cell.info = lists[indexPath.section]
         cell.releaseLabel.text = "发布了动态"
+        cell.indexPath = indexPath
+        cell.delegate = self
         return cell
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let vc = FriendViewController()
+        let obj = lists[indexPath.section]
+        let vc = DynamicDetailViewController()
+        let cell = tableView.cellForRowAtIndexPath(indexPath) as! DynamicCell
+        vc.dynamicObj = obj
+        vc.shareImage = cell.bigImgView.image
         navigationController?.pushViewController(vc, animated: true)
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return UITableViewAutomaticDimension
+    }
+}
+
+extension FollowViewController: DynamicCellDelegate, FollowProtocol {
+    func dynamicCellTapShare(sender: UIButton, indexPath: NSIndexPath) {
+        let dy = self.lists[indexPath.section]
+        let cell = tableView.cellForRowAtIndexPath(indexPath) as! DynamicCell
+        var type: YGShareType = .DYVisitor
+        let shareModel = YGShareModel()
+        shareModel.shareNickName = "分享自\"\(dy.name)\"的纯氧作品, 一起来看~"
+        if UserSingleton.sharedInstance.userId == "\(dy.userId)" {
+            type = .DYHost
+            shareModel.shareNickName = "我的纯氧作品, 一起来看~"
+        }else if !isEmptyString(UserSingleton.sharedInstance.userId) {
+            type = .DYHost
+        }
+        shareModel.shareID = "index.html#trends-details?listId=\(dy.id)"
+        shareModel.shareImage = cell.bigImgView.image
+        let sharetuple = YGShareHandler.handleShareInstalled(type)
+        share = YGShare(frame: CGRectZero, imgs: sharetuple.images, titles: sharetuple.titles)
+        share.shareModel = shareModel
+        share.animation()
+    }
+    
+    func dynamicCellTapPraise(sender: UIButton, indexPath: NSIndexPath) {
+        if UserSingleton.sharedInstance.isLogin() {
+            let object = lists[indexPath.section]
+            if sender.selected {
+                Server.cancelLike("\(object.id)", handler: { (success, msg, value) in
+                    if success {
+                        LogInfo("取消点赞")
+                        object.likeCount = object.likeCount - 1
+                        object.isLike = false
+                        sender.selected = false
+                        if object.likeCount <= 0 {
+                            sender.setTitle("赞TA", forState: .Normal)
+                        } else {
+                            sender.setTitle("\(object.likeCount)", forState: .Normal)
+                        }
+                        self.lists[indexPath.section] = object
+                    } else {
+                        SVToast.showWithError(msg!)
+                    }
+                })
+            } else {
+                Server.like("\(object.id)", handler: { (success, msg, value) in
+                    if success {
+                        LogInfo("点赞成功")
+                        object.likeCount = object.likeCount + 1
+                        object.isLike = true
+                        sender.selected = true
+                        sender.setTitle("\(object.likeCount)", forState: .Normal)
+                        self.lists[indexPath.section] = object
+                    } else {
+                        SVToast.showWithError(msg!)
+                    }
+                })
+            }
+        } else {
+            let logView = YGLogView()
+            logView.animation()
+            logView.tapLogViewClosure({ (type) in
+                LogInHelper.logViewTap(self, type: type)
+            })
+        }
+        
+    }
+    
+    func dynamicCellTapComment(sender: UIButton, indexPath: NSIndexPath) {
+        if UserSingleton.sharedInstance.isLogin() {
+            let obj = lists[indexPath.section]
+            let vc = DynamicDetailViewController()
+            vc.dynamicObj = obj
+            vc.isComment = true
+            navigationController?.pushViewController(vc, animated: true)
+        } else {
+            let logView = YGLogView()
+            logView.animation()
+            logView.tapLogViewClosure({ (type) in
+                LogInHelper.logViewTap(self, type: type)
+            })
+        }
+    }
+    
+    // 关注按钮点击
+    func dynamicCellTapFollow(sender: UIButton, indexPath: NSIndexPath) {
+        if UserSingleton.sharedInstance.isLogin() {
+            let object = lists[indexPath.section]
+            Server.followUser("\(object.userId)") { (success, msg, value) in
+                if success {
+                    self.modifyFollow(sender)
+                    object.follow = 1
+                    self.lists[indexPath.section] = object
+                } else {
+                    guard let m = msg else {return}
+                    SVToast.showWithError(m)
+                }
+            }
+        } else {
+            let logView = YGLogView()
+            logView.animation()
+            logView.tapLogViewClosure({ (type) in
+                LogInHelper.logViewTap(self, type: type)
+            })
+        }
     }
 }
